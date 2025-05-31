1234035e5f0178ace40107fffb83790f06ef293e
Waddenn <tpatelas@proton.me>
2025-05-31 17:11:23 +0200

    Add Nix expressions for building Miru on Darwin and Linux, update package metadata, and enhance update script for version and hash management

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Miru - Stream anime torrents in real-time";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-darwin" "x86_64-darwin"] (system:
+        f {
+          inherit system;
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    packages = forAllSystems ({pkgs, ...}: {
+      default = pkgs.callPackage ./default.nix {};
+    });
+
+    apps = forAllSystems ({
+      pkgs,
+      system,
+      ...
+    }: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/miru";
+      };
+    });
+
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        buildInputs = [pkgs.nodejs pkgs.yarn];
+      };
+    });
+  };
+}
